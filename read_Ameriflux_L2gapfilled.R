12b8db08163b4b77adb69e68d6ebb11c8028a587
davidjpmoore <davidjpmoore@email.arizona.edu>
2015-03-13 11:25:05 -0700

    Removed hardcoded setwd() and filenames to make more general. Just point it to your fluxdata folder and push go.

--- read_Ameriflux_L2gapfilled.R
+++ read_Ameriflux_L2gapfilled.R
@@ -6,19 +6,20 @@
 # set working directory
 #data is located on iPlant and available from Ameriflux
 #setwd("D:/Sites_DOE/AmeriFlux/Niwot Ridge/L2_gap_filled/V008") #Francesc's data structure
-setwd("C:/Users/dmoore1/Dropbox/rProjectsShare/Snow/data/FluxData/") #Dave's data structure
-
+#setwd("C:/Users/dmoore1/Dropbox/rProjectsShare/Snow/data/FluxData/") #Dave's data structure laptop
+#setwd("D:/Dropbox/rProjectsShare/Snow")  #Dave's data structure desktop
 #Remove ALL DATAFRAMES FROM THE ENVIRIONMENT 
 rm (list=ls()) #the only way I could work out how to bind all the new files together is to create a list of all dataframes in the environment after the loop | for safety I clear all variables at the outset.
 # 
 
 # read all the .csv files in the working directory (L2 gap filled) available for NR1 (1998-2013)
-tempFilelist = list.files(pattern="*.csv")
+#USES RELATIVE PATH to data/FluxData
+tempFilelist = list.files(path = "data/FluxData/",pattern="*.csv")
 
 #loop through each element of the list of files and assigns until length(tempFilelist)
 #can't figure out how to read lines 18 and 19 as headers using this approach
-for (i in 1:length(tempFilelist)) {
-  assign(substr(tempFilelist[i], 5, 14), read.csv(tempFilelist[i],skip=19,header=FALSE, na.strings=c('-9999','-6999'), stringsAsFactors=FALSE))  
+for (i in 1:3) {
+  assign(substr(tempFilelist[i], 5, 14), read.csv(file = paste0("data/FluxData/",tempFilelist[i]),skip=20,header=FALSE, na.strings=c('-9999','-6999'), stringsAsFactors=FALSE))  
 }
 
 
@@ -39,25 +40,40 @@ finddataframes <- function(envir = globalenv()) {
 allDataFrames <- finddataframes(globalenv()) # or just finddataframes()
 #combines all the dataframes using rbind
 #names the new dataframe with Ameriflux code max 5 characters (without the years)
-assign(substr(tempFilelist[1], 5, 9), do.call("rbind", allDataFrames))
+# assign(substr(tempFilelist[1], 5, 9), do.call("rbind", allDataFrames))
+assign("aggFluxtemp", do.call("rbind", allDataFrames))
 
 #name that I just assigned to the dataframe
 CombinedDataFrame=substr(tempFilelist[1], 5, 9)
 
 #Read header from file
-AmFluxheader=read.csv(tempFilelist[1],skip=17, nrows=1 ,header=FALSE, na.strings=c('-9999','-6999'),stringsAsFactors=FALSE)
+AmFluxheader=read.csv(file = paste0("data/FluxData/",tempFilelist[i]),skip=17, strip.white=TRUE, nrows=1 ,header=FALSE, na.strings=c('-9999','-6999'),stringsAsFactors=FALSE)
 AmerifluxCols = as.list(AmFluxheader)
 
 #Read Units
-AmFluxUnits= read.csv(tempFilelist[i],skip=18, nrows=1 ,header=FALSE, na.strings=c('-9999','-6999'), stringsAsFactors=FALSE)
+AmFluxUnits= read.csv(file = paste0("data/FluxData/",tempFilelist[i]),skip=18, nrows=1 ,header=FALSE, na.strings=c('-9999','-6999'), stringsAsFactors=FALSE)
 
-
+colnames(AmFluxUnits)=AmerifluxCols
 #add column names to new dataframe
-colnames(USNR1) <- AmerifluxCols #works but I'd rather not hard code 'USNR1'
-
+#colnames(USNR1) <- AmerifluxCols #works but I'd rather not hard code 'USNR1'
+colnames(aggFluxtemp) <- AmerifluxCols #works but I'd rather not hard code 'USNR1'
+assign(substr(tempFilelist[1], 5, 9), aggFluxtemp)
 
 #     save (USNR1, file="USNR1_cat.dat", ascii = TRUE)
 #     write.csv(USNR1, file="USNR1.csv", quote=FALSE, row.names=FALSE)
 #     save(USNR1, file="USNR1.rda")
+
+#Creating output file for EddyData
+# 
+# Year  DoY	Hour	NEE	LE	H	Rg	Tair	Tsoil	rH	VPD	Ustar
+# -	-	-	umolm-2s-1	Wm-2	Wm-2	Wm-2	degC	degC	%	hPa	ms-1
+# 1998	1	0.5	-1.21	1.49	-11.77	0	7.4	4.19	55.27	4.6	0.72
+# 1998	1	1	1.72	3.8	-13.5	0	7.5	4.2	55.95	4.6	0.52
+# 1998	1	1.5	-9999	1.52	-18.3	0	7.1	4.22	57.75	4.3	0.22
+# 1998	1	2	-9999	3.94	-17.47	0	6.6	4.23	60.2	3.9	0.2
+# 1998	1	2.5	2.55	8.3	-21.42	0	6.6	4.22	59.94	3.9	0.33
+# 
+# Eddyout = c(aggFluxtemp$Year)
+
 
 
