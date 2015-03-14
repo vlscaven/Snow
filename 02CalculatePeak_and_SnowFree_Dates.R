70cd5e1694c57bad9f6010639170e1630c8c3a6f
davidjpmoore <davidjpmoore@arizona.edu>
2015-03-14 14:57:05 -0700

    Added precipitation totals to the calculation.
    next process should use SNOTELsummary.rda

--- 02CalculatePeak_and_SnowFree_Dates.R
+++ 02CalculatePeak_and_SnowFree_Dates.R
@@ -13,6 +13,7 @@
 #setwd("D:/Dropbox/rProjectsShare/Snow/data/FluxData/") 
 #define object SWE as the SWE in Inches from the SNOWnwt dataframe
 SWE = SNOWnwt$SWE_in
+PrecAcc_in=SNOWnwt$PrecAcc_in
 #define Yind (year index) from the first to the last Water Year
 Yind=(min(wateryear):max(wateryear))
 #limits
@@ -23,14 +24,16 @@ syear=min(Yind)
 #allocate size of each of the quantities to be calculated
 snowpeaks = rep(1,length(Yind))
 PeakSWEday = rep(1,length(Yind))
-#PeakSWEDate = rep(1,length(Yind))
 ZeroSWEday = rep(1,length(Yind))
+PrecAcc_in_total = rep(1,length(Yind))
+
 ##
 ## Loop to calculate similar quantities on all years
 
 for(t in 1:length(Yind)){
   Yt = t-1+syear #Yt used to just convert from timestep of loop (t) to a year
   snowpeaks[t]=max(SWE[wateryear==(Yt)]) #within each wateryear calculate max SWE
+  PrecAcc_in_total[t]=max(PrecAcc_in[wateryear==(Yt)])
   tempdate =rDate[wateryear==Yt] #assign a temporary date (this wateryear)
   tempSWE =SWE[wateryear==Yt] #Assign a temporary SWE for this wateryear
   PeakSWEday[t]=max((tempdate[tempSWE==(snowpeaks[t])])) #calculate the LATEST date of max SWE within this year
@@ -44,6 +47,8 @@ doyPeakSWE= as.numeric(strftime(PeakSWEDate, format = "%j")) #convert to Day of 
 doyZeroSWE= as.numeric(strftime(ZeroSWEdate, format = "%j")) #convert to Day of Year
 
 #Saved 4 columns Yind, PeakSWEdate, ZeroSWEDate and MaxSWE_in(snowpeaks renamed) 
+
+SNOTELsummary =data.frame(Yind,snowpeaks, PrecAcc_in_total, PeakSWEDate, ZeroSWEdate)
 # keeps=c("Yind", "PeakSWEDate", "ZeroSWEDate", "MaxSWE_in")
 #  SNOTELsummary=SNOTELtotals[keeps]
 #  save(SNOTELsummary,file="data/SNOTELsummary.rda")
