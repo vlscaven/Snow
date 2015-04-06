205dbd2558ab9a45f108530105ca057f7dd3fd3e
vlscaven <vlscaven@email.arizona.edu>
2015-04-05 21:27:31 -0700

    fixes to harmonize data names, playing with plots and correlations

--- 01ReadSNOTEdata.R
+++ 01ReadSNOTEdata.R
@@ -8,35 +8,36 @@ SNOWnwt = read.table("data/SNOTELNiwot Tue Mar 10 2015.txt", head=TRUE, sep=",")
 #SNOTELNiwot Tue Mar 10 2015.txt was downloaded from SNOTEL site see README file
 
 #asking R to read the date as a date;
-rDate=as.Date(SNOWnwt$Date)
+rDate=as.Date(Niwotdaily$Date)
 
 #create variable "Year" from the date
 Year=as.numeric(format(rDate, "%Y"))
 
 #calculating annual snow totals
-snowpeaks_ann=tapply(SNOWnwt$SWE_in,Year,max)
-snowsum_ann=tapply(SNOWnwt$SWE_in,Year,sum)
+snowpeaks_ann=tapply(Niwotdaily$Snow.Water.Equivalent..in.,Year,max)
+snowsum_ann=tapply(Niwotdaily$Snow.Water.Equivalent..in,Year,sum)
 
 
 
 #defining the wateryear and waterdate
-waterdate=rDate+90
+waterdate=rDate+92
 wateryear=as.numeric(format(waterdate, "%Y"))
 
 
 Yind=(min(wateryear):max(wateryear))
-barplot(tapply(SNOWnwt$SWE_in,Year,sum))
-
+barplot(tapply(Niwotdaily$Snow.Water.Equivalent..in.,Year,sum))
+barplot(snowpeaks_ann)
+barplot(snowsum_ann)
 #caculate wateryear snow totals
-snowpeaks=tapply(SNOWnwt$SWE_in,wateryear,max)
-snowsum=tapply(SNOWnwt$SWE_in,wateryear,sum)
-prec_acc=tapply(SNOWnwt$PrecAcc_in,wateryear,sum)
+snowpeaks=tapply(Niwotdaily$Snow.Water.Equivalent..in.,wateryear,max)
+snowsum=tapply(Niwotdaily$Snow.Water.Equivalent..in.,wateryear,sum)
+prec_acc=tapply(Niwotdaily$Precipitation.Accumulation..in.,wateryear,sum)
 
 
 #simple plots
-barplot(tapply(SNOWnwt$SWE_in,wateryear,max))
+barplot(tapply(Niwotdaily$Snow.Water.Equivalent..in.,wateryear,max))
 plot(snowsum,snowpeaks)
-
+cor(snowsum, snowpeaks)
 #quick plot showing how cummulative SWE for the year and water year differ
 plot(snowsum_ann,prec_acc)
 
