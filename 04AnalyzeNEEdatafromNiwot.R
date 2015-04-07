be5bfeb03619892c60bb05b495a3296f9825316a
vlscaven <vlscaven@email.arizona.edu>
2015-04-06 22:17:28 -0700

    typo fix

--- 04AnalyzeNEEdatafromNiwot.R
+++ 04AnalyzeNEEdatafromNiwot.R
@@ -1,4 +1,4 @@
-plto#Purpose: Create annual, weekly totals for NEE for Niwot Ridge Ameriflux and compare to published estimates from Knowles et al | also plot time series of NEE vs Snowmelt
+#Purpose: Create annual, weekly totals for NEE for Niwot Ridge Ameriflux and compare to published estimates from Knowles et al | also plot time series of NEE vs Snowmelt
 #Author: davidjpmoore@email.arizona.edu
 #Date: 03/11/2015
 #To Do: create rDate for this file
