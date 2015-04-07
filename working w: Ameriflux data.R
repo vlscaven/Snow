6bc3e4ca8bf23e7e7c458a13c425772c911162c0
vlscaven <vlscaven@email.arizona.edu>
2015-04-06 22:14:55 -0700

    formatting Ameriflux dates

--- working w: Ameriflux data.R
+++ working w: Ameriflux data.R
@@ -11,7 +11,29 @@ x <- c(1988.0, 1988.25, 1988.5, 1988.75)
 library(lubridate)
 (f <- format(date_decimal(x), "%d-%m-%Y"))
 
-#playing around
+#formatting dates
 library(lubridate)
-fluxDates<-format(date_decimal(Ameriflux$Time..decimal.year.), "%d-%m-%Y")
+fluxDates<-format(date_decimal(Ameriflux$Time..decimal.year.), "%Y-%m-%d")
 Ameriflux$fluxDates<-fluxDates
+fluxDates
+
+fDate<-as.Date(fluxDates)
+fDate
+
+fYear<-(format(fDate, "%Y"))
+Ameriflux$fYear<-fYear
+fYear
+fDate
+
+
+#finding mean NEE_fill for each year
+meanAnnNEE<-tapply(Ameriflux$NEE_fill..umol.m2.s., Ameriflux$fYear, mean )
+plot(Biomass, meanAnnNEE)
+
+plot(biomassYear, snowpeaks[Yind<2013], type="l", col="blue", ylab="")
+par(new=T)
+plot(biomassYear, Biomass, type="l", col="green",ylab="")
+par(new=F)
+
+summary(Biomass)
+Ameriflux$fYear
